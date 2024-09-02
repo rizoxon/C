f1fb879d8644b7a93f7e7d53f90dff9b0bcc5eaa
Muhammadrizo <madaminov.co@gmail.com>
2024-09-02 14:39:02 +0500

    some learning, switch

--- playground.c
+++ playground.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-
-void lower(char txt[]){
-	char lower[255];
-	for(int i = 0; txt[i] != '\0'; i++) lower[i] = (txt[i] >= 65 && txt[i] <= 90) ? txt[i] + 32 : txt[i];
-	printf("%s\n", lower);
-}
-
 int main(){
-	lower("AMINOFF");
+	char str[] = "hello  this is test  some tabs			lal			";
+	char newstr[300];
+	for(int i = 0; str[i] != '\0'; i++){
+		switch(str[i]){
+			case ' ':
+				newstr[i] = '_';
+				break;
+			case '\t':
+				newstr[i] = '>';
+				break;
+			default:
+				newstr[i] = str[i];
+				break;
+		}
+	}
+
+	printf("%s\n", newstr);
 	return 0;
 }
